c09fb3d061d54cef85ad4f1701681c9e5ec04eee
Flamingo <flamingolive007@gmail.com>
2025-12-25 20:10:32 -0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -5,7 +5,7 @@ export const config = {
     process.env.DATABASE_URI || process.env.MONGODB_URI || 'mongodb://localhost:27017/dev',
   cloud: './cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  masterKey: process.env.MASTER_KEY || 'myMasterKey', //Add your master key here. Keep it secret!
   clientKey: process.env.CLIENT_KEY || 'myClientKey',
   restAPIKey: process.env.REST_API_KEY || 'myRestApiKey',
   javascriptKey: process.env.JAVASCRIPT_KEY || 'myJavascriptKey',
@@ -18,7 +18,7 @@ export const config = {
   defaultPublicWriteAccess: true,
 
   liveQuery: {
-    classNames: ['Posts', 'Comments', 'Streaming', 'User', 'Installation'], // List of classes to support for query subscriptions
+    classNames: ['Posts', 'Notifications', 'Comments', 'Streaming', '_User', 'Installation'], // List of classes to support for query subscriptions
   },
   schema: {
     definitions: schemaDefinitions,
